49fc7cbed4972583263aa7b16183fc629537a301
tythompson <tythompson@gmail.com>
2016-07-05 14:52:59 -0400

    Can create pet sale

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,8 +1,24 @@
 class PetsController < ApplicationController
   def index
+    @pets = Pet.all
   end
 
   def new
+    @pet = Pet.new
   end
 
+  def create
+    @pet = Pet.new (approved_params)
+    if @pet.save
+      flash[:notice] = "Legal pet sale created!"
+      redirect_to pets_path
+    else
+      render :new
+    end
+  end
+
+private
+  def approved_params
+    params.require(:pet).permit(:species,:price,:description,:seller_id)
+  end
 end
