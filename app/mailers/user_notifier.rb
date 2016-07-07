fd379b632fb18f9f316074cd34450bd4bcaa1aea
tythompson <tythompson@gmail.com>
2016-07-07 16:26:38 -0400

    bug fix

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -6,6 +6,6 @@ class UserNotifier < ActionMailer::Base
     @user = user
     @species = species
     mail( :to => @user.email,
-    :subject => ' 'Seems Legit' Pets: ' + @species + ' is available!' )
+          :subject => @species + ' is available at Seems Legit Pets!' )
   end
 end
