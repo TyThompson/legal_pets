e434dc13962a147244f954cf238ea5fcc77340f1
tythompson <tythompson@gmail.com>
2016-07-07 12:56:23 -0400

    Revert "changes to emailer"
    This reverts commit c10a03fbd14389f5544f3ed4ff3231c92fc721e9.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  if ENV["SENDGRID_USERNAME"]
+    Pony.options = {
+      :via => :smtp,
+      :via_options => {
+        :address => 'smtp.sendgrid.net',
+        :port => '587',
+        :domain => 'legal-pets.herokuapp.com',
+        :user_name => ENV['SENDGRID_USERNAME'],
+        :password => ENV['SENDGRID_PASSWORD'],
+        :authentication => :plain,
+        :enable_starttls_auto => true
+      }
+    }
+  end
+
   def watchlist_check
     contact = Watchlist.where("price < ?", @pet.price).where(species: @pet.species)
       contact.each do |p|
@@ -10,7 +25,7 @@ class ApplicationController < ActionController::Base
                 :from => "friend@legal-pets.herokuapp.com",
                 :headers => { 'Content-Type' => 'text/html' },
                 :subject => "Pet available on 'Legal' Pets!",
-                :body => "The pet you were waiting for is available for #{@pet.price}"
+                :body => 'pet_available_email'
       end
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,27 +1,4 @@
 Rails.application.configure do
-  if ENV["SENDGRID_USERNAME"]
-    Pony.options = {
-      :via => :smtp,
-      :via_options => {
-        :address => 'smtp.sendgrid.net',
-        :port => '587',
-        :domain => 'legal-pets.herokuapp.com',
-        :user_name => ENV['SENDGRID_USERNAME'],
-        :password => ENV['SENDGRID_PASSWORD'],
-        :authentication => :plain,
-        :enable_starttls_auto => true
-      }
-    }
-  end
-  #Action Mailer
-  config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.raise_delivery_errors = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
